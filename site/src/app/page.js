3da2030a332c260a010217da239b92884b5b90b5
Reece Bailey <80913719+reecebaileyy@users.noreply.github.com>
2023-07-07 21:45:32 -0700

    Update

--- site/src/app/page.js
+++ site/src/app/page.js
@@ -9,30 +9,30 @@ import { FaYelp } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center sm:justify-start justify-center bg-black min-w-screen min-h-screen'>
-      <div className=' flex flex-row sm:mb-36 z-10'>
+    <main className='flex flex-col items-center justify-center bg-black min-w-screen min-h-screen'>
+      <div className=' flex flex-row sm:mb-10 md:mb-10 z-10'>
         <div
-          className='sm:hidden md:hidden min-w-9/12 flex flex-col z-50 items-start justify-center space-y-40 absolute inset-y-0 left-0 w-1/6 p-5'>
+          className='hover-icons sm:hidden md:hidden min-w-9/12 flex flex-col z-50 items-start justify-center space-y-40 absolute inset-y-0 left-0 w-1/6 p-5'>
           <Link
-            className='text-white font-basker animate-appearFromLeft1 text-2xl hover:opacity-200'
+            className='icon text-white font-basker animate-appearFromLeft1 text-2xl hover:opacity-200'
             href='/about'
           >
             DJ SATO
           </Link>
           <Link
-            className='text-white font-basker animate-appearFromLeft2 text-2xl'
+            className='icon text-white font-basker animate-appearFromLeft2 text-2xl'
             href='/weddings'
           >
             Weddings
           </Link>
           <Link
-            className='text-white font-basker animate-appearFromLeft3 text-2xl'
+            className='icon text-white font-basker animate-appearFromLeft3 text-2xl'
             href='/privateparties'
           >
             Private Parties
           </Link>
           <Link
-            className='text-white font-basker animate-appearFromLeft4 text-2xl'
+            className='icon text-white font-basker animate-appearFromLeft4 text-2xl'
             href='/testimonials'>
             Testimonials
           </Link>
@@ -46,9 +46,9 @@ export default function Home() {
         </Image>
       </div>
       <div
-        className='lg:hidden xl:hidden 2xl:hidden 3xl:hidden hover-icons grid grid-cols-2 auto-rows-auto justify-items-center place-items-center z-50 w-3/4 '>
+        className='lg:hidden xl:hidden 2xl:hidden 3xl:hidden hover-icons grid grid-cols-2 auto-rows-auto justify-items-center place-items-end z-50 w-3/4'>
         <Link
-          className='icon text-white sm:text-sm md:text-sm col-start-1 col-span-1 font-basker duration-50 text-2xl hover:opacity-200'
+          className='icon text-white sm:text-sm md:text-sm col-start-1 col-span-1 font-basker duration-50 text-2xl'
           href='/about'
         >
           DJ SATO
@@ -71,7 +71,7 @@ export default function Home() {
           Testimonials
         </Link>
       </div>
-      <div className='hover-icons absolute bottom-0 right-0 flex flex-row sm:flex-col md:flex-col sm:space-y-5 items-end justify-end space-x-3 mr-10 mb-10'>
+      <div className='hover-icons absolute bottom-0 right-0 flex flex-row sm:flex-col md:flex-col md:space-y-5 sm:space-y-5 items-end justify-end space-x-3 mr-10 mb-10'>
         <Link href='https://www.instagram.com/djsato_/' target='_blank'  className='icon text-white transition animate-fadeIn'>
           <PiInstagramLogoBold size={25} />
         </Link>
