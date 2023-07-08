a7afc82b2b40c9732d9ae48acc574d6f725b38c1
Reece Bailey <80913719+reecebaileyy@users.noreply.github.com>
2023-07-07 21:32:19 -0700

    Update

--- site/src/app/page.js
+++ site/src/app/page.js
@@ -14,25 +14,25 @@ export default function Home() {
         <div
           className='sm:hidden md:hidden min-w-9/12 flex flex-col z-50 items-start justify-center space-y-40 absolute inset-y-0 left-0 w-1/6 p-5'>
           <Link
-            className='text-white font-basker animate-appearFromLeft2 text-2xl hover:opacity-200'
+            className='text-white font-basker animate-appearFromLeft1 text-2xl hover:opacity-200'
             href='/about'
           >
             DJ SATO
           </Link>
           <Link
-            className='text-white font-basker animate-appearFromLeft3 text-2xl'
+            className='text-white font-basker animate-appearFromLeft2 text-2xl'
             href='/weddings'
           >
             Weddings
           </Link>
           <Link
-            className='text-white font-basker animate-appearFromLeft4 text-2xl'
+            className='text-white font-basker animate-appearFromLeft3 text-2xl'
             href='/privateparties'
           >
             Private Parties
           </Link>
           <Link
-            className='text-white font-basker animate-appearFromLeft5 text-2xl'
+            className='text-white font-basker animate-appearFromLeft4 text-2xl'
             href='/testimonials'>
             Testimonials
           </Link>
@@ -46,37 +46,46 @@ export default function Home() {
         </Image>
       </div>
       <div
-        className='lg:hidden xl:hidden 2xl:hidden 3xl:hidden grid grid-cols-2 auto-rows-auto justify-items-center place-items-center z-50 w-3/4'>
+        className='lg:hidden xl:hidden 2xl:hidden 3xl:hidden hover-icons grid grid-cols-2 auto-rows-auto justify-items-center place-items-center z-50 w-3/4 '>
         <Link
-          className='text-white sm:text-lg md:text-lg col-start-1 col-span-1 font-basker animate-appearFromLeft1 duration-50 text-2xl hover:opacity-200'
+          className='icon text-white sm:text-sm md:text-sm col-start-1 col-span-1 font-basker duration-50 text-2xl hover:opacity-200'
           href='/about'
         >
           DJ SATO
         </Link>
         <Link
-          className='text-white sm:text-lg md:text-lg col-start-2 col-span-1 font-basker animate-appearFromLeft2 duration-50 text-2xl'
+          className='icon text-white sm:text-sm md:text-sm col-start-2 col-span-1 font-basker duration-50 text-2xl'
           href='/weddings'
         >
           Weddings
         </Link>
         <Link
-          className='text-white sm:text-lg md:text-lg col-start-1 col-span-1 font-basker animate-appearFromLeft3 duration-50 text-2xl'
+          className='icon text-white sm:text-sm md:text-sm col-start-1 col-span-1 font-basker duration-50 text-2xl'
           href='/privateparties'
         >
           Private Parties
         </Link>
         <Link
-          className='text-white sm:text-lg md:text-lg col-start-2 col-span-1 font-basker animate-appearFromLeft4 duration-50 text-2xl'
+          className='icon text-white sm:text-sm md:text-sm col-start-2 col-span-1 font-basker duration-50 text-2xl'
           href='/testimonials'>
           Testimonials
         </Link>
       </div>
-      <div className='absolute bottom-0 right-0 flex flex-row sm:flex-col md:flex-col sm:space-y-5 items-end justify-end space-x-3 p-10'>
-        <PiInstagramLogoBold className='text-white transition hover:-translate-y-3 animate-fadeIn' size={25} />
-        <TfiLinkedin className='text-white transition hover:-translate-y-3 animate-fadeIn' size={25} />
-        <FaYelp className='text-white transition hover:-translate-y-3 animate-fadeIn' size={25} />
-        <PiTiktokLogoBold className='text-white transition hover:-translate-y-3 animate-fadeIn' size={25} />
+      <div className='hover-icons absolute bottom-0 right-0 flex flex-row sm:flex-col md:flex-col sm:space-y-5 items-end justify-end space-x-3 mr-10 mb-10'>
+        <Link href='https://www.instagram.com/djsato_/' target='_blank'  className='icon text-white transition animate-fadeIn'>
+          <PiInstagramLogoBold size={25} />
+        </Link>
+        <Link href='https://www.linkedin.com/in/shane-sato3/' target='_blank'  className='icon text-white transition animate-fadeIn'>
+          <TfiLinkedin size={25} />
+        </Link>
+        <Link href='https://www.yelp.com/biz/dj-sato-fullerton' target='_blank'  className='icon text-white transition animate-fadeIn'>
+          <FaYelp size={25} />
+        </Link>
+        <Link href='https://www.tiktok.com/@djsato_' target='_blank' className='icon text-white transition animate-fadeIn'>
+          <PiTiktokLogoBold size={25} />
+        </Link>
       </div>
+
 
     </main>
   )
